ac2ee89f9fc08544d6c7a99c2af9b72311a93d77
ian <ianshih2003@gmail.com>
2024-11-10 18:22:12 -0300

    Fix doctor registration

--- src/models/specialty/specialty.controller.ts
+++ src/models/specialty/specialty.controller.ts
@@ -1,9 +1,7 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { SpecialtyService } from './specialty.service';
-import { DoctorGuard } from 'src/auth/guards/doctor.guard';
 
 @Controller('specialty')
-@UseGuards(DoctorGuard)
 export class SpecialtyController {
   constructor(private readonly specialtyService: SpecialtyService) { }
 
